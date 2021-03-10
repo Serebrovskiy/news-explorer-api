0577e1f14fd5ca98fff05311bba61dcce84c6e90
Aleksandr Serebrovskiy <aleksandr-web@yandex.ru>
2021-03-10 21:27:10 +0300

    added access control for cors

--- app.js
+++ app.js
@@ -24,9 +24,11 @@ mongoose.connect(MONGODB, {
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://aleksandr-web.online');
-  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
   next();
 });
 
